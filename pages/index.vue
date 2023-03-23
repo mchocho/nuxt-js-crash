459c961aa77f4aa29128fe82d313d128762bcd8f
mchocho <sal3sworld@gmail.com>
2023-03-24 00:47:14 +0200

    Initialised app, removed app.vue file, created a few pages

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <h2>Home</h2>
+
+    <p>In this Nuxt lesson, you'll learn how to make page components, which Nuxt then automatically creates routes for. ⭐⭐ Watch the whole course now (without ads) on Net Ninja Pro:</p>
+
+    <p>In this Nuxt lesson, you'll learn how to make page components, which Nuxt then automatically creates routes for. ⭐⭐ Watch the whole course now (without ads) on Net Ninja Pro:</p>
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+ h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+
+  p {
+    margin: 20px 0
+  }
+</style>
