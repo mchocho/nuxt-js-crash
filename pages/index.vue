bd191fae628fd1a7425c8b3bce04029f9a799644
mchocho <sal3sworld@gmail.com>
2023-03-24 12:25:50 +0200

    The NuxtLink component is the new anchor and RouterLink tag

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,40 @@
 <template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <!-- 
+            <NuxtLink> component
+
+            ref: https://nuxt.com/docs/api/components/nuxt-link#nuxtlink
+
+            NuxtLink determines whether the link is internal or external 
+            and renders it accordingly with available optimizations
+            (prefetching, default attributes, etc).
+
+            It's a replacement for the <a> and <RouterLink> tags and performs
+            faster.
+          -->
+            <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/about">About</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/products">Products</NuxtLink>
+            </li>
+        </ul>
+      </nav>
+    </header>
+    
+    
     <h2>Home</h2>
-
+    
     <p>In this Nuxt lesson, you'll learn how to make page components, which Nuxt then automatically creates routes for. ⭐⭐ Watch the whole course now (without ads) on Net Ninja Pro:</p>
-
+    
     <p>In this Nuxt lesson, you'll learn how to make page components, which Nuxt then automatically creates routes for. ⭐⭐ Watch the whole course now (without ads) on Net Ninja Pro:</p>
+  </div>
 </template>
 
 <script>
