348cacb06f9b2a43ca66a7747ab3aec2b2c611fa
mchocho <sal3sworld@gmail.com>
2023-04-01 01:02:36 +0200

    After adding google icon fonts, we can populate the app with icons

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -5,11 +5,20 @@
         <img :src="product.image" :alt="`image of ${product.title}`" class="mx-auto my-7"/>
       </div>
       <div class="p-7">
-        <h2 class="text-4xl my-7">{{ product.title }}</h2>
-        <p class="text-xl my-7">Only ${{ product.price }}</p>
+        <div class="mr-2">
+          <h2 class="text-4xl my-7">{{ product.title }}</h2>
+          <p class="text-xl my-7">Only ${{ product.price }}</p>
+        </div>
 
-        <h3>Product description</h3>
-        <p>{{ product.description }}</p>
+        <button class="btn flex">
+          <i class="material-icons mr-2 ">add_shopping_cart</i>
+          <span>Add to cart</span>
+        </button>
+
+        <div class="mt-5">
+          <h3>Product description</h3>
+          <p>{{ product.description }}</p>
+        </div>
       </div>
     </div>
 
