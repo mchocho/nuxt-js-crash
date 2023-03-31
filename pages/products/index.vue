b90a236f1879e6fdf165783e1be98eb10164618d
mchocho <sal3sworld@gmail.com>
2023-03-31 20:49:53 +0200

    Included ProductCard component, product list page looks very stylish

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="grid grid-cols-4 gap-5">
       <template v-for="(product, index) in products" :key="index">
-          <NuxtLink :to="`/products/${product.id}`">{{product.title}}</NuxtLink>
+        <ProductCard :product="product" />
       </template>
     </div>
   </div>
